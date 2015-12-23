5c6bf299eb73777ccd83d4bb44f922e9e3427c2f
costajob <costajob@gmail.com>
2015-12-23 13:49:27 +0100

    Mike - completed parser and tests

--- lib/jda/parser.rb
+++ lib/jda/parser.rb
@@ -21,22 +21,25 @@ module Jda
     def filter!(skus: [], stores: [], md_flag: false)
       @cache.each do |file, thread|
         thread.value.select! do |row|
-          normalize_row!(row)
-          skus_cond = skus.empty? ? true : skus.include?(row[0])
-          stores_cond = stores.empty? ? true : stores.include?(row[1])
-          md_flag_cond = md_flag ? md_flag == row[14] : true
+          skus_cond = skus.empty? ? true : skus.include?(row[0].strip!)
+          stores_cond = stores.empty? ? true : stores.include?(row[1].strip!)
+          md_flag_cond = md_flag ? row[14] == "Y" : true
           skus_cond && stores_cond && md_flag_cond
         end
       end
     end
 
-    private
-
-    def normalize_row!(row)
-      [0, 1, 14].each do |i|
-        row[i].strip!
+    def report
+      @cache.each do |file, thread|
+        report_file = "#{File.basename(file, ".txt")}_report.csv"
+        Thread.new do
+          CSV.open(report_file, "w") do |report|
+            thread.value.each do |row|
+              report << row
+            end
+          end
+        end
       end
-      row[14] = row[14] == 'Y' ? true : false
     end
   end
 end
